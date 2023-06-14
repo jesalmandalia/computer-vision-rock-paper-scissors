427e0a165d6a733d2bdff5d639a8c8f526f8a325
Jesal Mandalia <jesal.paresh.mandalia@cern.ch>
2023-06-14 15:48:16 +0100

    Update choices to first letter being capitalised to fit feedback

--- manual_rps.py
+++ manual_rps.py
@@ -1,28 +1,28 @@
 import random
 
 def get_computer_choice():
-    computer_choice = random.choice(['rock', 'paper', 'scissors'])
+    computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
     return computer_choice
 
 def get_user_choice():
-    user_choice = input("Please enter your choice: ")
-    user_choice = user_choice.lower()
+    user_choice = input("Please enter your choice of Rock, Paper, Scissors: ")
+    #user_choice = user_choice.lower()
     return user_choice
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
         print("It is a tie!")
-    elif computer_choice == 'rock' and user_choice == 'paper':     
+    elif computer_choice == 'Rock' and user_choice == 'Paper':     
         print("You won!")
-    elif computer_choice == 'rock' and user_choice == 'scissors':
+    elif computer_choice == 'Rock' and user_choice == 'Scissors':
         print("You lost!")
-    elif computer_choice == 'paper' and user_choice == 'rock':     
+    elif computer_choice == 'Paper' and user_choice == 'Rock':     
         print("You lost!")
-    elif computer_choice == 'paper' and user_choice == 'scissors':
+    elif computer_choice == 'Paper' and user_choice == 'Scissors':
         print("You won!")
-    elif computer_choice == 'scissors' and user_choice == 'rock':
+    elif computer_choice == 'Scissors' and user_choice == 'Rock':
         print("You won!")
-    elif computer_choice == 'scissors' and user_choice == 'paper':
+    elif computer_choice == 'Scissors' and user_choice == 'Paper':
         print("You lost!")
     else:   
         print("Invalid input!") 
